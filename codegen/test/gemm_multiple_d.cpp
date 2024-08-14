a53f39c338d979d46b1d36793ca5dc7024b223b0
HTEC <>
2024-08-14 11:19:41 +0000

    Add hipRTC compilation to test

--- codegen/test/gemm_multiple_d.cpp
+++ codegen/test/gemm_multiple_d.cpp
@@ -21,7 +21,8 @@ std::vector<rtc::src_file> get_headers_for_test()
     auto hs = ck::host::GetHeaders();
     std::transform(
         hs.begin(), hs.end(), std::back_inserter(result), [&](const auto& p) -> rtc::src_file {
-            return {p.first, p.second};
+            std::string sec(p.second.begin(), p.second.end());
+            return {p.first, sec};
         });
     return result;
 }
@@ -130,10 +131,13 @@ const std::string gemm_compile_check = R"__ck__(
 
 extern "C" __global__ void f(const ck::half_t* a, const ck::half_t* b, ck::half_t* c) {
     using G = ${template};
-    constexpr auto desc = ${template}::make_descriptor(ck::make_naive_tensor_descriptor_packed(ck::make_tuple(${m}, ${k})),
-                                             ck::make_naive_tensor_descriptor(ck::make_tuple(${n}, ${k}), ck::make_tuple(1, ${n})),
-                                             ck::make_tuple(),
-                                             ck::make_naive_tensor_descriptor_packed(ck::make_tuple(${m}, ${n})));
+    constexpr auto desc =
+    G::make_descriptor(ck::make_naive_tensor_descriptor_packed(ck::make_tuple(${m},
+    ${k})),
+                                             ck::make_naive_tensor_descriptor(ck::make_tuple(${n},
+                                             ${k}), ck::make_tuple(1, ${n})), ck::make_tuple(),
+                                             ck::make_naive_tensor_descriptor_packed(ck::make_tuple(${m},
+                                             ${n})));
 
     static_assert(desc.IsValid(), "Invalid ck gemm.");
 
@@ -163,23 +167,31 @@ TEST_CASE(test_problem_kernel)
     std::string epilogue = "";
     std::string prologue = "";
 
-    for(auto solution : prob.GetSolutions("gfx90a", prologue, epilogue))
+    auto solutions = prob.GetSolutions("gfx90a", prologue, epilogue);
+    std::cout << "Num solutions: " << solutions.size() << std::endl;
+    for(auto i = 0; i < solutions.size(); ++i)
     {
-        auto src  = ck::host::InterpolateString(gemm_compile_check,
-                                               {{"include", prob.GetIncludeHeader()},
-                                                {"template", solution.ToTemplateString()},
-                                                {"m", std::to_string(prob.M)},
-                                                {"n", std::to_string(prob.N)},
-                                                {"k", std::to_string(prob.K)}});
-        auto srcs = get_headers_for_test();
+        std::cout << "Testing solution " << std::to_string(i + 1) << std::endl;
+        auto&& solution = solutions[i];
+        auto src        = ck::host::InterpolateString(gemm_compile_check,
+                                                      {{"include", prob.GetIncludeHeader()},
+                                                       {"template", solution.ToTemplateString()},
+                                                       {"m", std::to_string(prob.M)},
+                                                       {"n", std::to_string(prob.N)},
+                                                       {"k", std::to_string(prob.K)}});
+        auto srcs       = get_headers_for_test();
         srcs.push_back({"main.cpp", src});
-        rtc::compile_options options;
+        // rtc::compile_options options;
+        // options.kernel_name = "f";
+        rtc::hip_compile_options options;
         options.kernel_name = "f";
-        auto k              = rtc::compile_kernel(srcs, options);
-        auto block_size     = solution.GetTemplateParameter<std::size_t>("BlockSize");
-        auto m_per_block    = solution.GetTemplateParameter<std::size_t>("MPerBlock");
-        auto n_per_block    = solution.GetTemplateParameter<std::size_t>("NPerBlock");
-        auto grid_size      = ck::host::integer_divide_ceil(prob.M, m_per_block) *
+        // auto k              = rtc::compile_kernel(srcs, options);
+        std::cout << src << std::endl;
+        auto k           = rtc::compile_hip_code_object(src, options);
+        auto block_size  = solution.GetTemplateParameter<std::size_t>("BlockSize");
+        auto m_per_block = solution.GetTemplateParameter<std::size_t>("MPerBlock");
+        auto n_per_block = solution.GetTemplateParameter<std::size_t>("NPerBlock");
+        auto grid_size   = ck::host::integer_divide_ceil(prob.M, m_per_block) *
                          ck::host::integer_divide_ceil(prob.N, n_per_block);
         k.launch(nullptr, grid_size * block_size, block_size)(a.data(), b.data(), c.data());
 
